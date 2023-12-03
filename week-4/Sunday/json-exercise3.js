8d0a01e319998923abbbaadccca4536258f4bb49
Naarani <n3ra96@gmail.com>
2023-12-03 16:04:43 +0200

    finished json exercises

---
+++ week-4/Sunday/json-exercise3.js
@@ -0,0 +1,10 @@
+let animal = {
+    name:  string  ,
+    weight:  number,
+    habitat: string ,
+    endangered:  Boolean,
+    food:  array,
+    predator: array,
+    delicious: number
+
+}
---
+++ week-4/Sunday/json-exercise4.js
@@ -0,0 +1,11 @@
+let animal = {
+    "name":  "Squirrel" ,
+    "weight":  '12-26g',
+    "habitat": "every habitat" ,
+    "endangered":  false,
+    "food":  [  'Scots pine' , 'Douglas fir',  'Oak' , 'Beech', 'Hazel', 'Ash'],
+    "percentage-diet": [3.8, 6.2, 30, 5.5, 25, 1.9],
+    "predator": ['Predatory-birds', 'foxes', 'coyotes', 'bobcats', 'mountain-lions', 'raccoons', 'domestic-cats', 'snakes'],
+    "delicious": 3
+
+}
